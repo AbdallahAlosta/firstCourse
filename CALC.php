d1521161e175eba07d3a9162713d95c9e2a51429
Java <Java@Java-PC>
2014-06-25 22:45:49 -0800

    new commit

---
+++ CALC.php
@@ -0,0 +1,11 @@
+<?php
+function addNumber($value, $n) 
+{
+    if ($n == 0) {
+        return; 
+    }else
+    return $value / $n;
+}
+
+?>
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include './CALC.php';
+        $result = addNumber(5, 2);
+        echo "$result";
+        ?>
+    </body>
+</html>
